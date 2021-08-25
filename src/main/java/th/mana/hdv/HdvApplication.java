bff9d05ef048f46b8fdd7e1d25d3bc6c9af64bbd
thd-cuevas <ing.jdcuevas@gmail.com>
2021-08-25 18:45:48 -0500

    Subir Spring boot

---
+++ src/main/java/th/mana/hdv/HdvApplication.java
@@ -0,0 +1,13 @@
+package th.mana.hdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/th/mana/hdv/HdvApplicationTests.java
@@ -0,0 +1,13 @@
+package th.mana.hdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
